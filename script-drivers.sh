f0e89e3960a1a201e658836b7734223f4c407882
frandaurat <31714085+FranDaurat@users.noreply.github.com>
2023-11-11 14:07:26 -0300

    script-drivers.sh

--- script-drivers.sh
+++ script-drivers.sh
@@ -1,8 +1,8 @@
-sudo apt update
-sudo apt upgrade
+sudo apt update -y 
+sudo apt upgrade -y 
 sudo apt dist-upgrade -y
 sudo apt install realtek-rtl88xxau-dkms
-sudo apt install dkms
+sudo apt install dkms -y 
 git clone https://github.com/aircrack-ng/rtl8812au
 cd rtl8812au/
 make
